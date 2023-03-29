59bf6468e359a473d956525a67897e276a8bee16
trout6516 <aalto65@duck.com>
2023-03-29 21:38:15 +0100

    Created Variables File

--- variables_data_types/bin/variables_data_types.dart
+++
@@ -1,5 +0,0 @@
-import 'package:variables_data_types/variables_data_types.dart' as variables_data_types;
-
-void main(List<String> arguments) {
-  print('Hello world: ${variables_data_types.calculate()}!');
-}
