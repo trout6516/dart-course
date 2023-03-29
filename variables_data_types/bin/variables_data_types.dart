07c543fbd96d73e48075230fd8d4c19d95327d7e
trout6516 <aalto65@duck.com>
2023-03-29 03:30:10 +0100

    Variables and Data Types

---
+++ variables_data_types/bin/variables_data_types.dart
@@ -0,0 +1,5 @@
+import 'package:variables_data_types/variables_data_types.dart' as variables_data_types;
+
+void main(List<String> arguments) {
+  print('Hello world: ${variables_data_types.calculate()}!');
+}
---
+++ variables_data_types/test/variables_data_types_test.dart
@@ -0,0 +1,8 @@
+import 'package:variables_data_types/variables_data_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
